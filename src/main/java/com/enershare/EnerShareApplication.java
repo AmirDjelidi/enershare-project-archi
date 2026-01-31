1c5b82afa952e9e6c9411b651d9b040956fd5448
Pooca <amir.djelidi@outlook.fr>
2026-01-31 14:03:51 +0100

    Upload Files (Init)

---
+++ src/main/java/com/enershare/EnerShareApplication.java
@@ -0,0 +1,13 @@
+package com.enershare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnerShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnerShareApplication.class, args);
+	}
+
+}
